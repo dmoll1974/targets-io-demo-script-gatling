8dbced4a9719e22e5fda13c2ae93c1ce249653c3
x090371 <Daniel.Moll@klm.com>
2016-07-01 10:30:51 +0200

    added port

--- src/test/scala/com/gatling/demo/gatling/helpers/Assertions.scala
+++ src/test/scala/com/gatling/demo/gatling/helpers/Assertions.scala
@@ -11,7 +11,7 @@ import io.gatling.http.Predef._
 class Assertions extends Simulation{
 
 
-  var targetsIoUrl = "http://targetsio"
+  var targetsIoUrl = "http://targetsio:3000"
   var productName = System.getProperty("productName")
   var dashboardName = System.getProperty("dashboardName")
   var testRunId = System.getProperty("testRunId")
