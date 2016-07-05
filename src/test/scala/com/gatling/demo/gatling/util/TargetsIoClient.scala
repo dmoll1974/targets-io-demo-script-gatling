4a5ec76c0cdab25adb64beb5ef007f70335903cc
x090371 <Daniel.Moll@klm.com>
2016-07-05 11:43:45 +0200

    fixed success value

--- src/test/scala/com/gatling/demo/gatling/util/TargetsIoClient.scala
+++ src/test/scala/com/gatling/demo/gatling/util/TargetsIoClient.scala
@@ -62,7 +62,7 @@ object TargetsIoClient {
             tries = tries + 1
           } else {
             println("Giving up after 5 attempts... please fix manually afterwards in the targets-io dashboard GUI.")
-            success = 200
+            success = true
           }
 
 
