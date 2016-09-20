3623da1f58350d1ec5b24afb3fff26e77815b7e5
x090371 <Daniel.Moll@klm.com>
2016-09-20 14:33:49 +0200

    added pipeline groovy file

---
+++ src/targets-io-pipeline.groovy
@@ -0,0 +1,19 @@
+node('test') {
+
+    def testRunId = env.JOB_NAME + "-" + env.BUILD_NUMBER
+    def buildUrl = env.BUILD_URL
+
+    // Get the maven tool.
+    // ** NOTE: This 'M3' maven tool must be configured
+    // **       in the global configuration.
+    def mvnHome = tool 'M3'
+
+
+//   stage 'Execute load test'
+
+    // Run the test
+    sh "${mvnHome}/bin/mvn clean install gatling:execute -Pacc -Pnightly -Passertions -DtestRunId=$testRunId -DbuildResultsUrl=$buildUrl"
+
+
+
+}
